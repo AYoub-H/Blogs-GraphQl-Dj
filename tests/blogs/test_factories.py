31581739764c67afd09335296bf84432f60eca6f
AYoub-H <himichayoub01@gmail.com>
2022-03-13 23:30:55 +0100

    configure tests

---
+++ tests/blogs/test_factories.py
@@ -0,0 +1,8 @@
+# from tests.blogs.factories import UserFactory
+# from blogs.models import User
+#
+#
+# def test_user_model():
+#     user = UserFactory()
+#     assert user.id
+#     assert isinstance(User, user)
